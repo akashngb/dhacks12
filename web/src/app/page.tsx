cdfbfcb81e79117adf68fefac94bd34139527cf6
Srivathsan Prasanna <SrivPrasanna@gmail.com>
2026-01-10 23:46:28 -0500

    feat: try to add gemini api call

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -9,7 +9,15 @@ import ExpandedMap from "../components/bento/widgets/ExpandedMap";
 import ExpandedEmpty from "../components/bento/widgets/ExpandedEmpty";
 import { SignedIn } from "@clerk/nextjs";
 import Link from "next/link";
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import ExpandedChat from "../components/bento/widgets/ExpandedChat";
 
 export default function Home() {
   const [expandedCard, setExpandedCard] = useState<string | null>(null);
@@ -21,9 +29,9 @@ export default function Home() {
         <div className="bento-card p-4 rounded-2xl overflow-hidden row-span-3 col-span-3 row-start-2 col-start-9 bg-[#F8F4E3] animate-slide-up fill-mode-[both] [animation-delay:0.2s]"></div>
         <div className="bento-card p-4 rounded-2xl overflow-hidden row-span-2 col-span-4 row-start-2 col-start-12 bg-[#F8F4E3] animate-slide-up fill-mode-[both] [animation-delay:0.3s]"></div>
         <div className="bento-card p-4 rounded-2xl overflow-hidden row-span-2 col-span-4 row-start-6 col-start-2 bg-[#F8F4E3] animate-slide-up fill-mode-[both] [animation-delay:0.4s]"></div>
-        {/* <div className="bento-card p-4 rounded-2xl row-span-3 col-span-6 row-start-5 col-start-6 bg-[#F8F4E3] animate-slide-up fill-mode-[both] [animation-delay:0.5s] overflow-hidden"> */}
+        <div className="bento-card p-4 rounded-2xl row-span-3 col-span-6 row-start-5 col-start-6 bg-[#F8F4E3] animate-slide-up fill-mode-[both] [animation-delay:0.5s] overflow-hidden">
           <MapBox className="row-span-3 col-span-6 row-start-5 col-start-6 " />
-        {/* </div> */}
+        </div>
         <div className="bento-card p-4 rounded-2xl overflow-hidden row-span-4 col-span-4 row-start-4 col-start-12 bg-[#F8F4E3] animate-slide-up fill-mode-[both] [animation-delay:0.6s]"></div>
       </div>
 
